a3a163d377774b9ee0f2a9539440465943bb372c
Maksimilian <maksimilian.ceresniovyj@locatory.com>
2022-07-28 10:22:49 +0300

    Add basic form UI with input fields

--- src/pages/root/Root.tsx
+++ src/pages/root/Root.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import InputField from '../../atoms/input-field/InputField';
+import Typography, { ElementType } from '../../atoms/typography/Typography';
+
+import styles from './Root.module.scss';
 
 const Root: React.FC = () => {
-    return <div>Root</div>;
+    return (
+        <div className={styles.container}>
+            <Typography text="Create form" elementType={ElementType.H1} />
+            <InputField name="fullname" label="Fullname:" onChangeInput={() => {}} />
+            <InputField name="email" label="Email:" onChangeInput={() => {}} />
+        </div>
+    );
 };
 
 export default Root;
