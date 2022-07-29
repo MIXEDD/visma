115ebb7c62f81ab819eb40412f644df20ffbc9ad
Maksimilian <maksimilian.ceresniovyj@locatory.com>
2022-07-29 16:07:52 +0300

    Reducer refactor

--- src/store/tree/reducer.ts
+++ src/store/tree/reducer.ts
@@ -19,43 +19,37 @@ export interface TreeState extends TreeData {}
 
 const initialState: TreeState = INITIAL_STATE;
 
-const setNode = (treeData: TreeData, node: NodeForInsertion) => {
-    const clonedTreeState = R.clone(treeData);
-    insertNodeToParent(clonedTreeState, node);
-
-    return clonedTreeState;
-};
-
-const deleteNode = (treeData: TreeData, fullName: string) => {
-    let clonedTreeState: TreeData | null = R.clone(treeData);
-
-    const result = deleteParentNode(clonedTreeState, fullName);
-
-    if (result === null) {
-        return null;
-    }
-
-    if (result) {
-        return clonedTreeState;
-    }
-
-    deleteTreeNode(clonedTreeState, fullName);
-
-    return clonedTreeState;
-};
-
 export function treeReducer(state: TreeState | null = initialState, action: TreeAction) {
     switch (action.type) {
         case SET_NODE: {
             if (state) {
-                state = setNode(state, (action as SetNodeAction).payload);
+                const clonedTreeState = R.clone(state);
+                insertNodeToParent(clonedTreeState, (action as SetNodeAction).payload);
+
+                return clonedTreeState;
             }
 
             return state;
         }
         case DELETE_NODE: {
+            const fullName = (action as DeleteNodeAction).payload;
+
             if (state) {
-                state = deleteNode(state, (action as DeleteNodeAction).payload);
+                let clonedTreeState: TreeData | null = R.clone(state);
+
+                const result = deleteParentNode(clonedTreeState, fullName);
+
+                if (result === null) {
+                    return null;
+                }
+
+                if (result) {
+                    return clonedTreeState;
+                }
+
+                deleteTreeNode(clonedTreeState, fullName);
+
+                return clonedTreeState;
             }
 
             return state;
@@ -68,7 +62,7 @@ export function treeReducer(state: TreeState | null = initialState, action: Tree
 
                 orderTreeNode(clonedTreeState, fullName);
 
-                state = clonedTreeState;
+                return clonedTreeState;
             }
 
             return state;
